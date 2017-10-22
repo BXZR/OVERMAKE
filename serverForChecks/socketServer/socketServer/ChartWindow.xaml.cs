39fa3bba8903a03fba4d771dd8a09a76b0b29fd2
BXZR <405181821@qq.com>
2017-10-23 04:53:39 +0800

    第二种判步方法扩展
    使用皮尔逊相关系数做匹配
    同时生成样本和对比的双曲线图
    此外简单修改了采样方法细节

--- serverForChecks/socketServer/socketServer/ChartWindow.xaml.cs
+++ serverForChecks/socketServer/socketServer/ChartWindow.xaml.cs
@@ -53,7 +53,7 @@ namespace socketServer
             {
                 case UseDataType.accelerometerY:
                     {
-                        title.Text = "Y轴加速度";
+                        title.Text = "Z轴加速度";
                     }
                     break;
                 case UseDataType.compassDegree:
@@ -154,6 +154,138 @@ namespace socketServer
             theChartGrid.Children.Add(gr);
         }
         #endregion
+
+
+
+        #region 折线图2
+        //绘制对比曲线使用的折线图
+        public void CreateChartSpline2(UseDataType IN, List<double> theValues , List<double>  theValues2)
+        {
+            //创建一个图标
+            Chart chart = new Chart();
+
+            //设置图标的宽度和高度
+            chart.Width = 800;
+            chart.Height = 425;
+            chart.Margin = new Thickness(5, 5, 10, 5);
+            //是否启用打印和保持图片
+            chart.ToolBarEnabled = false;
+
+            //设置图标的属性
+            chart.ScrollingEnabled = false;//是否启用或禁用滚动
+            chart.View3D = true;//3D效果显示
+
+            //创建一个标题的对象
+            Title title = new Title();
+
+            title.Text = "表格标题";
+            //设置标题的名称
+            switch (IN)
+            {
+                case UseDataType.accelerometerZ:
+                    {
+                        title.Text = "Z轴加速度";
+                    }
+                    break;
+                case UseDataType.compassDegree:
+                    {
+                        title.Text = "磁力计角度";
+                    }
+                    break;
+            }
+
+            title.Padding = new Thickness(0, 10, 5, 0);
+
+            //向图标添加标题
+            chart.Titles.Add(title);
+
+            //初始化一个新的Axis
+            Axis xaxis = new Axis();
+            //设置Axis的属性
+            //图表的X轴坐标按什么来分类，如时分秒
+            xaxis.IntervalType = IntervalTypes.Seconds;
+            //图表的X轴坐标间隔如2,3,20等，单位为xAxis.IntervalType设置的时分秒。
+            xaxis.Interval = 1;
+            //设置X轴的时间显示格式为7-10 11：20           
+            //   xaxis.ValueFormatString = "MM秒";
+            //给图标添加Axis            
+            chart.AxesX.Add(xaxis);
+
+            Axis yAxis = new Axis();
+            //设置图标中Y轴的最小值永远为0           
+            yAxis.AxisMinimum = -2;
+            //设置图表中Y轴的后缀 
+            switch (IN)
+            {
+                case UseDataType.accelerometerY:
+                    {
+                        yAxis.Suffix = "m/s2";
+                    }
+                    break;
+                case UseDataType.compassDegree:
+                    {
+                        yAxis.Suffix = "o";
+                    }
+                    break;
+            }
+            chart.AxesY.Add(yAxis);
+            // 创建一个新的数据线。               
+            DataSeries dataSeries = new DataSeries();
+            DataSeries dataSeries2 = new DataSeries();
+            // 设置数据线的格式。
+            switch (IN)
+            {
+                case UseDataType.accelerometerZ:
+                    {
+                        dataSeries.LegendText = "Z轴加速度 ";
+                        dataSeries2.LegendText = "Z轴加速度样本";
+                    }
+                    break;
+                case UseDataType.compassDegree:
+                    {
+                        dataSeries.LegendText = "磁力计角度";
+                    }
+                    break;
+            }
+
+
+            dataSeries.RenderAs = RenderAs.Spline;//折线图
+            dataSeries.XValueType = ChartValueTypes.Auto;
+
+            dataSeries2.RenderAs = RenderAs.Spline;//折线图
+            dataSeries2.XValueType = ChartValueTypes.Auto;
+            // 设置数据点              
+            DataPoint dataPoint;
+            DataPoint dataPoint2;
+            for (int i = 0; i < theValues.Count; i++)
+            {
+                // 创建一个数据点的实例。                   
+                dataPoint = new DataPoint();
+                dataPoint2 = new DataPoint();
+                // 设置X轴点                    
+                dataPoint.XValue = (double)i / 4;
+                dataPoint2.XValue = (double)i / 4;
+                //设置Y轴点                   
+                dataPoint.YValue = theValues[i];
+                dataPoint2.YValue = theValues2[i];
+                dataPoint.MarkerSize = 8;
+                dataPoint2.YValue = theValues2[i];
+                dataSeries.DataPoints.Add(dataPoint);
+                dataSeries2.DataPoints.Add(dataPoint2);
+            }
+
+            // 添加数据线到数据序列。                
+            chart.Series.Add(dataSeries);
+            chart.Series.Add(dataSeries2);
+
+            //将生产的图表增加到Grid，然后通过Grid添加到上层Grid.           
+            Grid gr = new Grid();
+            gr.Children.Add(chart);
+
+            theChartGrid.Children.Add(gr);
+        }
+        #endregion
+
 
     }
 }
--- serverForChecks/socketServer/socketServer/stepDetection.cs
+++ serverForChecks/socketServer/socketServer/stepDetection.cs
@@ -17,29 +17,35 @@ namespace socketServer
 
         //来自一篇论文
         //主要思想是检查重复元素的信息，如果信息被认为是重复的，就认为走了一步
-        //-----------------------这个方法最大的难点就是对第一步的采样------------------------//
-        private  int indexForStep = 3;//记录下一个数值的坐标，每一次从上一步开始比对就可以了
-        //此外还表示1条的数据被抛弃了
+        //-----------------------这个方法的难点:对第一步的采样，判断是否匹配------------------------//
+
         private  double minusGate = 0.4;//如果数据差异百分比超过10%就认为数据是不一样的
         private  int countBetweenTwoStep = 3;//两步之间最少的数据量
 
         public  bool isSampled = false;//是否已经采样完毕
         public List<double> sample = new List<double>();//被采集的样本（波峰检测单元做第一个波形的检测）
 
-        private int dataDropCount = 5;//前几个数据不要了
+        private int dataDropCount = 1;//前几个数据不要了
+        private int indexForSTART= 0;//记录的初始的波峰的下标
 
         private void makeSample(List<double> wave)
         {
+            
             sample = new List<double>();
             if (wave.Count < dataDropCount)
                 return; //前几个数据不要了，会有很大的误差
 
             int stepNumber = 0;
             int direction = wave[dataDropCount] > 0 ? -1 : 1;
-            for (int i = 3; i < wave.Count - 1; i++)
+            for (int i = dataDropCount ; i < wave.Count - 1; i++)
             {
                 double minus = wave[i + 1] - wave[i];
-                sample.Add(wave[i]);
+
+                //获得第一个波峰之间的数据用作样本
+                if (stepNumber == 1 )
+                {
+                   sample.Add(wave[i]);
+                }
                 if (minus * direction > 0)//放弃突变的情况
                 {
                     direction *= -1;
@@ -47,19 +53,24 @@ namespace socketServer
                     {
                         //"波峰"
                         stepNumber++;
-
+                        if (stepNumber == 1)
+                        {
+                            indexForSTART = i;//重新纪录抛弃项目，这也是后续计算的开始位置
+                        }
                         //判断出来一个波峰+波谷就认为结束了
                         if (stepNumber >=2)
                         {
-                            isSampled = true;
                             Console.WriteLine("sample over");
                             countBetweenTwoStep = sample.Count;
 
                             for (int y = 0; y < sample.Count; y++)
                                 Console.WriteLine("采样结果" + sample[y]);
 
-
+                            ChartWindow sampleShow = new socketServer.ChartWindow();
+                            sampleShow.Show();
+                            sampleShow.CreateChartSpline(UseDataType.accelerometerZ, sample);
                             Console.WriteLine("countBetweenTwoStep: " + countBetweenTwoStep);
+                            isSampled = true;
                             break;
                         }
 
@@ -80,50 +91,37 @@ namespace socketServer
             if (isSampled == false)
             {
                 makeSample(AZValues);
+               
             }
             else
             {
-                //int i = AZValues.Count - countBetweenTwoStep;
-                //if (i <= indexForStep)
-                //    return;
-
-                ////包装到两个队列中使用各种方法来判断相似程度
-                //List<double> data1 = new List<double>();
-                ////固定样本，并且原本样本可以用data1替换（时机合适的话）
-                //for ( ;  i < AZValues.Count; i++)
-                //{
-                //    data1.Add(AZValues[i]);
-                //}
-
-
-                //if (contrast1(data1 , sample))
-                //{
-                //    Console.WriteLine("判断走了一步");
-                //    peackBuff.Add(AZValues.Count - 1);
-                //    indexForStep = AZValues.Count;//做一个缓冲标记
-                //}
-
-                peackBuff = new List<int>();
-                for (int i = dataDropCount; i < AZValues.Count; i+= countBetweenTwoStep)
+               // Console.WriteLine("第一个波峰的位置：" + dataDropCount);
+                peackBuff = new List<int>();//记录下标的位置
+                for (int i = indexForSTART; i < AZValues.Count; i+= countBetweenTwoStep/2)
                 {
                     if ((i + countBetweenTwoStep) > AZValues.Count)
                     {
-                        break;//数据不够就不进行计算了
+                        //数据不够就不进行计算了
+                        //数据采集完成就可以继续计算
+                        break;
                     }
                     List<double> data1 = new List<double>();
                     for (int j = i; j < i + countBetweenTwoStep; j++)
                     {
+                        //Console.WriteLine("AZ: " + AZValues[j]);
                         data1.Add(AZValues[j]);
                     }
-                    if (contrast1(data1, sample))
+                    if (contrast2(data1, sample))
                     {
+                        Console.WriteLine("---------" + peackBuff.Count);
                         Console.WriteLine("判断走了一步");
-                        peackBuff.Add(i);
-                        indexForStep = AZValues.Count;//做一个缓冲标记
+                        peackBuff.Add(i);  
                     }
                 }
             }
         }
+
+        bool showed = false;
 
         //对比两个序列的相似程度方法1（最土鳖的数值方法）
         //返回真则认为两组数据差不多
@@ -132,21 +130,92 @@ namespace socketServer
             int sameCount = 0;
             for(int i = 0; i < data1.Count; i++)
             {
+                //在这里减少1是因为实验用的手机传感器存在偏差1
                 if ( Math.Abs(  Math.Abs(data1[i])-1 ) < 0.01 || Math.Abs(data1[i]) < 0.01)
                     continue;
-                //为了预防除零异常，分子分母分别加上了一个1
-                    double checkUp = Math.Abs(Math.Abs(data1[i]) - Math.Abs(data2[i])) ;
+
+                    double checkUp = Math.Abs(data1[i] - data2[i]) ;
                     double checkValue = Math.Abs(checkUp / (Math.Abs(data2[i])));
-                     Console.WriteLine("Value --- "+Math.Abs(data1[i]) +" --- "+ Math.Abs(data2[i]));
+                    Console.WriteLine("Value --- "+data1[i] +" --- "+ data2[i] +" --- ");
                     Console.WriteLine("checkValue = " + checkValue);
                     if (checkValue > minusGate)
                     sameCount++;
             }
             Console.WriteLine("sameCount2  = " + sameCount);
             if (sameCount >= countBetweenTwoStep / 2)
+            {
+                if (showed == false)
+                { 
+                ChartWindow show = new socketServer.ChartWindow();
+                show.Show();
+                show.CreateChartSpline2(UseDataType.accelerometerZ, data1, data2);
+                    showed = true;
+                }
                 return true;
+            }
             return false;
         }
+
+        //对比两个序列的相似程度方法2（皮尔逊相关系数的方法）
+        //返回真则认为两组数据差不多
+        /*
+            0.8-1.0     极强相关
+            0.6-0.8     强相关
+            0.4-0.6     中等程度相关
+            0.2-0.4     弱相关
+            0.0-0.2     极弱相关或无相关 
+         */
+
+        private int showPWindowCount = 5;//匹配成功显示的曲线窗口数量
+        bool contrast2(List<double> data1, List<double> data2)
+        {
+
+            if (data1.Count == 0)
+                return false;
+
+            double dataAverage1 = 0;//数据的平均数
+            double dataAverage2 = 0;//数据平均数2
+            for (int i = 0; i < data1.Count; i++)
+            {
+                dataAverage1 += data1[i];
+                dataAverage2 += data2[i];
+            }
+
+            dataAverage1 /= data1.Count;
+            dataAverage2 /= data2.Count;
+             
+            //分成多段来写，保持清晰
+            double up = 0;//分子
+            double  data1Down = 0;//分母计算因子
+            double data2Down = 0;//分母计算因子
+            for (int i = 0; i < data1.Count; i++)
+            {
+                up += (data1[i] - dataAverage1) * (data2[i] - dataAverage2);
+                data1Down += (data1[i] - dataAverage1) * (data1[i] - dataAverage1);
+                data2Down += (data2[i] - dataAverage2) * (data2[i] - dataAverage2);
+            }
+            double down = Math .Sqrt(  data1Down * data2Down);
+            Console.WriteLine("data1Down = "+ data1Down);
+            if (down == 0 ||  ( data1Down/data1 .Count) < 0.2 )//不可比较的话统统返回false
+                return false;
+
+            double pearsonValue = up / down;
+            Console.WriteLine("pearsonValue = " + pearsonValue);
+            if (Math.Abs( pearsonValue)  > 0.75)//一个折中的小数值（数值要求并不是很严格）
+            {
+                if (showPWindowCount >0 )
+                {
+                    ChartWindow show = new socketServer.ChartWindow();
+                    show.Show();
+                    show.CreateChartSpline2(UseDataType.accelerometerZ, data1, sample);
+                    showPWindowCount--;
+                }
+                return true;
+            }
+            return false;
+        }
+
+
         //对比两个字符串的相似度
         private static decimal getsimilaritywith(string sourcestring, string str)
         {
@@ -169,7 +238,6 @@ namespace socketServer
         //这个类的统一外部刷新接口，因为有分组的存在，这个是非常必要的
         public  void makeFlash()
         {
-            indexForStep = 0;
             peackBuff.Clear();
         }
 

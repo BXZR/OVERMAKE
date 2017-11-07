caef17fc34bf2535d4fa65fe78fca91573747ca0
BXZR <405181821@qq.com>
2017-11-08 05:57:36 +0800

    VR模式客户端（2）

---
+++ demoForPhone/demoForPhone/demoForPhone/demoForPhone/New Unity Project/Assets/codes/ForData/inputChange.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class inputChange : MonoBehaviour {
+
+	//使用面板回调来调用这个方法，并不常用，考虑泛用性的功能
+	public void changeServerIP()
+	{
+		server.serverIP = this.GetComponent <InputField> ().text;
+	}
+}

46203a0b41c128aaf0e80dc4bf35aa316e81a0b8
BXZR <405181821@qq.com>
2017-11-21 10:13:14 +0800

    3D模式修正
    修复因上次3D模式迁移到手机被破坏的PC3D模式。
    为PC3D模式增加拖尾路径使得定位效果更加明显。

---
+++ showMoveGames/showForMoves/Assets/codes/systemValues.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class systemValues : MonoBehaviour {
+
+  //简单记录一下信息
+	public static 	string linkServerLabel = "-------";
+	public static int stepCountNow = 0;//记录走过多少步
+	public static double stepLengthNow  = 0; //当前步长
+	public static  double stepAngle = 0;//当前角度
+	public static double slopNow = 0;//当前姿态的一个小小的推断，如果比较大就认为是在奔跑
+	public static bool canFlashPosition = false;//是否可以更新人物坐标
+}

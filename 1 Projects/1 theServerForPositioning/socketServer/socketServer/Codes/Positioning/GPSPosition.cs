8d397d9cef17856fd0a0f770bfc51b1c4eea5da1
BXZR <405181821@qq.com>
2018-06-19 15:14:18 +0800

    GPS联合定位的思路在确定
    GPS联合定位的思路在确定
    虽说没写啥代码
    但是思路正在逐渐清晰，需要一个接收GPS，计算偏差度和反馈GPS的过程

---
+++ 1 Projects/1 theServerForPositioning/socketServer/socketServer/Codes/Positioning/GPSPosition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace socketServer.Codes.Positioning
+{
+    //这个类专门用来处理GPS的信息
+    //如果能够获得GPS信号并且做相关判别
+    //在有GPS信号的时候能够判断GPS信号的效果
+    //GPS能用就混合定位
+   public  class GPSPosition
+    {
+        
+
+
+    }
+
+
+    public class GPS
+    {
+        public float GPSX = 0;
+        public float GPSY = 0;
+
+        public string getGPSPositionInformation()
+        {
+            return "GPS: (" + GPSX +"," +GPSY + ")";
+        }
+    }
+}
